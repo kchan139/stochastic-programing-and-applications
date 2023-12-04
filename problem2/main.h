349b21ac47a64bdf1dc3b2ecf14009d1e61c00b5
Khoa Tran <khoatrandd.139@gmail.com>
2023-12-04 10:03:45 +0700

    LON ME MAY QUAN

--- problem2/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include<bits/stdc++.h>
-#include<string>
-
-using namespace std;
-
-#endif
---
+++ scripts/problem2/main.cpp
@@ -0,0 +1,11 @@
+
+#include "main.h"
+#include "EdmondKarp.cpp"
+
+int main(int argc, char *argv[])
+{
+    string fileName = "test.txt";
+    playGround(fileName);
+
+    return 0;
+}
